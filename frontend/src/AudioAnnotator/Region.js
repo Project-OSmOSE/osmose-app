4d8d568cb02265ba43b7f8e3f3ee642831588b50
Qouagga <72140989+Qouagga@users.noreply.github.com>
2023-03-30 16:59:11 +0200

    fix(Annotator) : Can delete box on top canva (#55)

--- frontend/src/AudioAnnotator/Region.js
+++ frontend/src/AudioAnnotator/Region.js
@@ -21,7 +21,21 @@ type RegionProps = {
   onAddAnotherAnnotation: (Annotation) => void,
 };
 
-class Region extends Component<RegionProps> {
+type RegionState = {spectrogramEnd: number};
+
+class Region extends Component<RegionProps, RegionState> {
+
+  constructor(props: RegionProps) {
+    super(props);
+
+    this.state = {
+      spectrogramEnd:0,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({spectrogramEnd: parseInt(document.querySelector('.canvas-wrapper').style.width) });
+  }
 
   playAnnotation = () => {
     this.props.onRegionPlayed(this.props.annotation);
@@ -41,6 +55,7 @@ class Region extends Component<RegionProps> {
 
   render() {
     const isActive: boolean = this.props.annotation.active;
+    const distanceToMarginLeft: number = this.state.spectrogramEnd - Math.floor(this.props.offsetLeft);
 
     const duration: number = this.props.annotation.endTime - this.props.annotation.startTime;
     const freqRange: number = this.props.annotation.endFrequency - this.props.annotation.startFrequency;
@@ -48,18 +63,22 @@ class Region extends Component<RegionProps> {
     const width: number = Math.floor(this.props.timePxRatio * duration);
     const height: number = Math.floor(this.props.freqPxRatio * freqRange) + HEADER_HEIGHT + HEADER_MARGIN;
 
+    const headerPositionIsTop = this.props.offsetTop > HEADER_HEIGHT + HEADER_MARGIN ? true : false;
+
     const styles = {
       wrapper: {
         left: Math.floor(this.props.offsetLeft),
-        top: Math.floor(this.props.offsetTop) - HEADER_HEIGHT - HEADER_MARGIN,
+        top: Math.floor(this.props.offsetTop) - (headerPositionIsTop ? HEADER_HEIGHT : 0)- HEADER_MARGIN,
         width: width,
         height: height,
       },
       header: {
         height: HEADER_HEIGHT,
-        marginBottom: HEADER_MARGIN,
+        marginTop: (headerPositionIsTop ? 0 : HEADER_MARGIN),
+        marginBottom: (headerPositionIsTop ? HEADER_MARGIN : 0 ),
         backgroundColor: isActive ? this.props.color : `${this.props.color}88`,
         border: isActive ? `2px solid ${this.props.color}` : `2px solid ${this.props.color}88`,
+        marginLeft: distanceToMarginLeft > 50 ? '50%': `-${50-distanceToMarginLeft}px`,
       },
       headerSpan: {
         height: `${HEADER_HEIGHT}px`,
@@ -69,36 +88,38 @@ class Region extends Component<RegionProps> {
         height: height - HEADER_HEIGHT - HEADER_MARGIN,
       },
     };
+    const regionBody = (
+      <React.Fragment>
+        <div
+          className="region-body"
+          style={styles.body}
+        ></div>
+      </React.Fragment>
+      )
 
     return (
       <div
         className="region"
         style={styles.wrapper}
       >
-        <p
-          className="d-flex region-header"
-          style={styles.header}
-        >
-          <button
-            className="btn-simple fa fa-play-circle"
-            onClick={this.playAnnotation}
-          ></button>
-          <span
-            className="flex-fill text-center"
-            onClick={this.selectAnnotation}
-            style={styles.headerSpan}
-          >{this.props.annotation.annotation}</span>
-          <button
-            className="btn-simple fa fa-times-circle"
-            onClick={this.deleteAnnotation}
-          ></button>
-        </p>
-
-        <div
-          className="region-body"
-          style={styles.body}
-          onPointerDown={this.addAnotherAnnotation}
-        ></div>
+        {headerPositionIsTop ? "" : regionBody}
+        <p className = "d-flex region-header"
+          style = { styles.header }>
+            <button
+              className="btn-simple fa fa-play-circle"
+              onClick={this.playAnnotation}
+            ></button>
+            <span
+              className="flex-fill text-center"
+              onClick={this.selectAnnotation}
+              style={styles.headerSpan}
+            >{this.props.annotation.annotation}</span>
+            <button
+              className="btn-simple fa fa-times-circle"
+              onClick={this.deleteAnnotation}
+            ></button>
+        </p >
+        {headerPositionIsTop ? regionBody : ""}
       </div>
     );
   }
