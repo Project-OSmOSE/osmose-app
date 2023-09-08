5ed940a67957f757d3d877c76fa75f863fd714b3
Erwan Keribin <erwan.keribin@gmail.com>
2023-09-08 11:07:39 +0200

    Manage new timestamp CSV header

--- backend/api/actions/datawork_import.py
+++ backend/api/actions/datawork_import.py
@@ -132,9 +132,8 @@ def datawork_import(*, wanted_datasets, importer):
         dataset_files = []
         # Create dataset_files
         with open(audio_folder / "timestamp.csv", encoding="utf-8") as csvfile:
-            for timestamp_data in csv.reader(csvfile):
-                filename, start_timestamp = timestamp_data
-                start = parse_datetime(start_timestamp)
+            for timestamp_data in csv.DictReader(csvfile):
+                start = parse_datetime(timestamp_data["timestamp"])
                 audio_metadatum = AudioMetadatum.objects.create(
                     start=start,
                     end=(
@@ -147,10 +146,10 @@ def datawork_import(*, wanted_datasets, importer):
                 dataset_files.append(
                     DatasetFile(
                         dataset=curr_dataset,
-                        filename=filename,
+                        filename=timestamp_data["filename"],
                         filepath=settings.DATASET_FILES_FOLDER
                         / dataset["conf_folder"]
-                        / filename,
+                        / timestamp_data["filename"],
                         size=0,
                         audio_metadatum=audio_metadatum,
                     )
