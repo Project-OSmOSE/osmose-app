acbf1aa02704e246f97c4c6e3876a80783046783
Romain Vovard <romain.vovard@gmail.com>
2023-11-08 12:29:40 +0100

    chore(frontend): initialize aplose frontend with Vite instead of deprecated create-react-app (create / move / delete files)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+      "/backend": "http://localhost:8000",
+    },
+  },
+  plugins: [react()],
+});
