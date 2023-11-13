3dc01789c2a1b4ac1d1b6901a840e028be8e7c17
Dewi GLEAU <dewi.gleau@free.fr>
2023-11-13 12:40:11 +0100

    add News test

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+# pylint: disable-next=line-too-long
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
