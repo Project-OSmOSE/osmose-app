263a5019773e54fc9d89e20170591b61c3b76618
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2023-11-30 16:17:16 +0100

    [Website] Update Footer component style

--- website/src/components/Layout/Footer/index.tsx
+++ website/src/components/Layout/Footer/index.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-
-import './styles.css';
+import { IonIcon } from "@ionic/react";
+import { logoGithub, mailOutline } from "ionicons/icons";
 import ifremer from '../../../img/logo/logo_ifremer_blanc_267_250.webp';
 
-export const Footer: React.FC = () => {
-  return (
-<footer id="main-footer" className="footer d-flex flex-wrap flex-column flex-md-row justify-content-around align-items-center py-5">
-    <p className="m-3">
-      Powered by <br/>
-      <img src={ifremer} alt="Ifremer logo"/>
-    </p>
+import './styles.css';
 
-    <p className="m-3">
-      Contact : <br />
-      <a href="mailto:contact-osmose@ensta-bretagne.fr" title="Contact OSmOSE"> contact-osmose@ensta-bretagne.fr</a>
-    </p>
-    <p className="m-3">
-      OSmOSE <a href="/humans.txt" title="Full credits"> credits</a>, <br /> GPL-3.0, 2021
-    </p>
-    <p className="m-3">
-      <a href="https://github.com/Project-OSmOSE">GitHub</a>
-    </p>
-</footer>
-  );
-}
+
+export const Footer: React.FC = () => (
+    <footer>
+        <div className="ifremer">
+            <p>Powered by</p>
+            <img src={ ifremer } alt="Ifremer"/>
+        </div>
+
+        <div className="links">
+            <a className="link" href="https://github.com/Project-OSmOSE">
+                <IonIcon icon={ logoGithub }></IonIcon>
+                Github
+            </a>
+            <a className="link" href="mailto:contact-osmose@ensta-bretagne.fr" title="Contact OSmOSE">
+                <IonIcon icon={ mailOutline }></IonIcon>
+                contact-osmose@ensta-bretagne.fr
+            </a>
+        </div>
+
+        <div className="license">
+            <p>OSmOSE <a href="/humans.txt" title="Full credits"> credits</a>,</p>
+            <p>GPL-3.0, 2021</p>
+        </div>
+    </footer>
+)
