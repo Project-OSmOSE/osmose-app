4c89926483213bf833ccd55d2477a3e39ea20e01
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2023-12-06 16:08:42 +0100

    [Back & website] show collaborators on home

--- backend/osmosewebsite/tests/views/collaborator.py
+++ backend/osmosewebsite/tests/views/collaborator.py
@@ -23,6 +23,16 @@ class CollaboratorViewSetTestCase(APITestCase):
         self.assertEqual(response.data[0]["name"], "name2")
         self.assertEqual(response.data[0]["show_on_home_page"], False)
 
+    def test_on_home(self):
+        """CollaboratorViewSet 'list' returns the list of Collaborator to show on home page"""
+        url = reverse("collaborators-on_home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(list(response.data[0].keys()), CollaboratorFields)
+        self.assertEqual(response.data[0]["show_on_home_page"], True)
+        self.assertEqual(response.data[1]["show_on_home_page"], True)
+
     def test_retrieve(self):
         """CollaboratorViewSet 'retrieve' returns Collaborator details"""
         url = reverse("Collaborator-detail", kwargs={"pk": 1})
--- backend/osmosewebsite/views/collaborator.py
+++ backend/osmosewebsite/views/collaborator.py
@@ -1,5 +1,5 @@
 """OSmOSE Website Views - Collaborator"""
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, decorators, response
 from backend.osmosewebsite.models.collaborator import Collaborator
 from backend.osmosewebsite.serializers.collaborator import CollaboratorSerializer
 
@@ -12,3 +12,10 @@ class CollaboratorViewSet(viewsets.ModelViewSet):
     queryset = Collaborator.objects.all()
     serializer_class = CollaboratorSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @decorators.action(detail=False)
+    def on_home(self, request):
+        """List collaborators to be shown on home page"""
+        queryset = Collaborator.objects.all().filter(show_on_home_page=True)
+        serializer = self.serializer_class(queryset, many=True)
+        return response.Response(serializer.data)
