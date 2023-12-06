3a98787fdcfd18443630d4e43fce9bb8e4c2081e
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2023-12-06 10:54:01 +0100

    Clean CI

--- backend/osmosewebsite/serializers/team_member.py
+++ backend/osmosewebsite/serializers/team_member.py
@@ -2,22 +2,24 @@
 from rest_framework import serializers
 from backend.osmosewebsite.models.team_member import TeamMember
 
+TeamMemberFields = [
+    "id",
+    "name",
+    "position",
+    "biography",
+    "picture",
+    "mail_address",
+    "research_gate_url",
+    "personal_website_url",
+    "github_url",
+    "linkedin_url",
+    "is_former_member",
+]
+
 
 class TeamMemberSerializer(serializers.ModelSerializer):
     """Serializer meant to output TeamMember data"""
 
     class Meta:
         model = TeamMember
-        fields = [
-            "id",
-            "name",
-            "position",
-            "biography",
-            "picture",
-            "mail_address",
-            "research_gate_url",
-            "personal_website_url",
-            "github_url",
-            "linkedin_url",
-            "is_former_member",
-        ]
+        fields = TeamMemberFields
--- backend/osmosewebsite/tests/views/team_member.py
+++ backend/osmosewebsite/tests/views/team_member.py
@@ -1,10 +1,9 @@
 """User DRF-Viewset test file"""
 
 from django.urls import reverse
-from backend.osmosewebsite.models import TeamMember
-
 from rest_framework import status
 from rest_framework.test import APITestCase
+from backend.osmosewebsite.serializers.team_member import TeamMemberFields
 
 
 class TeamMemberViewSetTestCase(APITestCase):
@@ -32,22 +31,7 @@ class TeamMemberViewSetTestCase(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
-        self.assertEqual(
-            list(response.data[0].keys()),
-            [
-                "id",
-                "name",
-                "position",
-                "biography",
-                "picture",
-                "mail_address",
-                "research_gate_url",
-                "personal_website_url",
-                "github_url",
-                "linkedin_url",
-                "is_former_member",
-            ],
-        )
+        self.assertEqual(list(response.data[0].keys()), TeamMemberFields)
         self.assertEqual(response.data[0]["name"], "user2")
         self.assertEqual(response.data[0]["position"], "job2")
         self.assertEqual(response.data[0]["is_former_member"], False)
@@ -57,22 +41,7 @@ class TeamMemberViewSetTestCase(APITestCase):
         url = reverse("members-detail", kwargs={"pk": 3})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            list(response.data),
-            [
-                "id",
-                "name",
-                "position",
-                "biography",
-                "picture",
-                "mail_address",
-                "research_gate_url",
-                "personal_website_url",
-                "github_url",
-                "linkedin_url",
-                "is_former_member",
-            ],
-        )
+        self.assertEqual(list(response.data), TeamMemberFields)
         self.assertEqual(response.data["name"], "user3")
         self.assertEqual(response.data["position"], "job3")
         self.assertEqual(response.data["is_former_member"], True)
