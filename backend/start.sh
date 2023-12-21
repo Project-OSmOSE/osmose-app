5c9dd76d6efedb5e75d333094b1dc02c9f613ae5
Elodie <elodie.morin@ensta-bretagne.fr>
2023-12-21 17:15:25 +0100

    revert start.sh changes

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # If on staging we want some dev libraries like Faker for seeding
-if [ "$STAGING" = "true" ]
-then
-  echo ">>> Installing dev libraries"
-  poetry install
-fi
+if [ "$STAGING" = "true" ]; then poetry install; fi
 
 # Normal setup commands
-echo ">>> Setup"
 #poetry run python manage.py collectstatic --noinput # Since osmose.ifremer.fr is read-only, this won't work
 poetry run python manage.py migrate
 
 # Launching server
-echo ">>> Launching server"
 poetry run gunicorn -b 0.0.0.0:8000 backend.wsgi
