f7739a93dc35d728873d86b6c570f8276b481ea0
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2024-01-09 10:21:46 +0100

    Cypress cherry pick

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+import {defineConfig} from "cypress";
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+export default defineConfig({
+    env: {
+        aploseURL: 'http://localhost:5173/'
     },
-  },
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
 });
---
+++ frontend/cypress/e2e/0-osmose-tests/1 - login.cy.js
@@ -0,0 +1,27 @@
+describe('login page', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env('aploseURL'))
+    });
+
+    it('logs in with admin credentials', () => {
+        cy.get("#loginInput").click();
+        cy.get("#loginInput").type("admin");
+        cy.get("#passwordInput").click();
+        cy.get("#passwordInput").type("osmose29");
+
+        cy.contains("Submit").click()
+
+        cy.get("h1").eq(1).should('have.text', 'Annotation Campaigns')
+    });
+
+    it('logs in with invalid credentials', () => {
+        cy.get("#loginInput").click();
+        cy.get("#loginInput").type("admin");
+        cy.get("#passwordInput").click();
+        cy.get("#passwordInput").type("osmose");
+
+        cy.contains("Submit").click();
+
+        cy.get('.error-message').should('have.text', 'Access denied')
+    });
+})
