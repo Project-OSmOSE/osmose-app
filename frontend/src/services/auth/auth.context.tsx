390296604d955dabf494587f7132b02e6e48fbae
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2024-01-11 09:58:07 +0100

    wip - auth

--- frontend/src/services/auth/auth.context.tsx
+++ frontend/src/services/auth/auth.context.tsx
@@ -10,8 +10,8 @@ export interface AuthAction {
   token?: string;
 }
 
-const AuthContext: Context<Auth> = createContext<Auth>({});
-const AuthDispatchContext: Context<Dispatch<AuthAction> | undefined> = createContext<Dispatch<AuthAction> | undefined>(undefined);
+const _AuthContext: Context<Auth> = createContext<Auth>({});
+const _AuthDispatchContext: Context<Dispatch<AuthAction> | undefined> = createContext<Dispatch<AuthAction> | undefined>(undefined);
 
-export const useAuthContext = () => useContext(AuthContext);
-export const useAuthDispatch = () => useContext(AuthDispatchContext);
+export const useAuthContext = () => useContext(_AuthContext);
+export const useAuthDispatch = () => useContext(_AuthDispatchContext);
--- frontend/src/services/auth/auth.service.tsx
+++ frontend/src/services/auth/auth.service.tsx
@@ -2,14 +2,14 @@ import { useAuthContext, useAuthDispatch } from "./auth.context.tsx";
 import { Response } from "../../old/utils/requests.tsx";
 import { post } from "superagent";
 
-const URI = '/api/token/';
+const _URI = '/api/token/';
 
 export const useAuthService = () => {
   return {
     context: useAuthContext(),
     dispatch: useAuthDispatch(),
     login: (username: string, password: string): Response<string> => {
-    const request = post(URI).send({ username, password });
+    const request = post(_URI).send({ username, password });
     const response = new Promise<string>((resolve, reject) => {
       request.then(r => resolve(r.body.access)).catch(reject);
     });
