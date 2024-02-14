21c5acd5002986ad39310fd5b9d50e7be1f46fe0
ixio <erwan.keribin@gmail.com>
2024-02-14 16:36:36 +0100

    chore(CI): Fix CI cypress testing (#187)

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -26,9 +26,20 @@
 
 Cypress.Commands.add('login', (username, password) => {
   cy.visit(Cypress.env('aploseURL'))
-  cy.get("#loginInput").click();
   cy.get("#loginInput").type(username);
-  cy.get("#passwordInput").click();
   cy.get("#passwordInput").type(password);
   cy.contains("Submit").click()
+})
+
+// https://docs.cypress.io/api/commands/session#Updating-an-existing-login-helper-function
+Cypress.Commands.add('session_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.login(username, password)
+  }, {
+    // https://www.cypress.io/blog/2023/02/08/mistake-when-using-cy-session-and-how-to-solve-it
+    validate() {
+      cy.document().its('cookie').should('contain', 'token')
+    }
+  })
+  cy.visit(Cypress.env('aploseURL'))
 })
