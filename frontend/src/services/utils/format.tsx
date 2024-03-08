3e448886682dc4b45709b71c0b05b6489ebde89c
Elodie <elodie.morin@ensta-bretagne.fr>
2024-03-08 10:59:35 +0100

    Dataset list page - use Ion buttons - add spinner while loading - use Ion toasts
    Dataset API Service
    - dates as string for store serialization
    
    New Toast service for Ion toast use
    Update buildErrorMessage

--- frontend/src/services/utils/format.tsx
+++ frontend/src/services/utils/format.tsx
@@ -13,7 +13,29 @@ export function formatTimestamp(rawSeconds: number, withMs: boolean = true) {
 }
 
 export function buildErrorMessage(e: any): string {
-  if (e !== null && typeof e === 'object' && e.status && e.message) {
+  if (e !== null && typeof e === 'object' && e.status && (e.message || e.response)) {
+    if (e.response) {
+      if (e.response.body) {
+        const message = Object
+          .entries(e.response.body)
+          .map(([key, value]) => Array.isArray(value) ? `${ key }: ${ value.join(' - ') }` : '')
+          .join("\n");
+        if (message.length > 0) return message;
+        try {
+          return JSON.stringify(e.response.body)
+        } catch (e) {
+          console.warn(e)
+        }
+      }
+      if (e.response.text) {
+        try {
+          return JSON.stringify(e.response.text)
+        } catch (e) {
+          console.warn(e)
+        }
+        return e.response.text
+      }
+    }
     return 'Status: ' + e.status.toString() +
       ' - Reason: ' + e.message +
       (e.response.body?.title ? ` - ${ e.response.body.title }` : '') +
