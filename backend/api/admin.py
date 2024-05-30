bba79f31e6829505c2f97f89798e6496f4afc38b
Elodie <elodie.morin@ensta-bretagne.fr>
2024-05-30 10:21:31 +0200

    Upgrade metadatax v°

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,11 +2,12 @@
 # Python admin has too many false-positives on the following warnings:
 # pylint: disable=too-many-function-args, R0801
 
-from django.contrib import admin, messages
 from django import forms
+from django.contrib import admin
+from django.contrib import messages
+from django.contrib.admin import sites
 from django.db import IntegrityError, transaction
 from django.utils.html import format_html
-
 
 from backend.api.models import (
     Dataset,
@@ -26,6 +27,66 @@ from backend.api.models import (
     ConfidenceIndicator,
     ConfidenceIndicatorSet,
 )
+
+
+class AploseAdminSite(admin.AdminSite):
+    app_ordering = {
+        "api": 1,
+        "metadatax": 2,
+        "auth": 2,
+        "osmosewebsite": 2
+    }
+    metadatax_ordering = {
+        "> Map": 0,
+        "Institutions": 1,
+        "Projects": 2,
+        "Deployments": 3,
+        "Channel configurations": 4,
+        "Files": 5,
+        "Equipment - Recorders": 10,
+        "Equipment - Hydrophones": 11,
+    }
+
+    def _build_app_dict(self, request, label=None):
+        # we create manually a dict to fake a model for our view 'map'
+        map_fake_model = {
+            'name': '> Map',
+            'admin_url': "/map/",
+            'object_name': 'Map',
+            'perms': {'delete': False, 'add': False, 'change': False},
+            'add_url': ''
+        }
+
+        # get the app dict from the parent method
+        app_dict = super()._build_app_dict(request, label)
+        # check if there is value for label = admin view for specific app
+        if label:
+            app_dict['models'] = [m for m in app_dict['models'] if m['name'] in list(self.metadatax_ordering.keys())]
+            app_dict['models'].append(map_fake_model)
+
+        # otherwise = home admin view with all apps
+        else:
+            app = app_dict.get('metadatax', None)
+            if app:
+                app['models'] = [m for m in app['models'] if m['name'] in list(self.metadatax_ordering.keys())]
+                app['models'].append(map_fake_model)
+                app['models'].sort(key=lambda x: self.metadatax_ordering[x['name']])
+        return app_dict
+
+    def get_app_list(self, request):
+        """
+        Return a sorted list of all the installed apps that have been
+        registered in this site.
+        """
+        app_dict = self._build_app_dict(request)
+        app_list = sorted(app_dict.values(), key=lambda x: self.app_ordering[x['app_label']])
+
+        return app_list
+
+
+mysite = AploseAdminSite()
+admin.site = mysite
+sites.site = mysite
 
 
 def get_many_to_many(obj, field_name, related_field_name="name"):
@@ -403,7 +464,6 @@ admin.site.register(AudioMetadatum, AudioMetadatumAdmin)
 admin.site.register(GeoMetadatum, GeoMetadatumAdmin)
 admin.site.register(SpectroConfig, SpectroConfigAdmin)
 admin.site.register(WindowType, WindowTypeAdmin)
-
 
 # admin.site.register(Collection, CollectionAdmin)
 # admin.site.register(TabularMetadatum, TabularMetadatumAdmin)
