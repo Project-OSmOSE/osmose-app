d935bef7882431c11bde456f5792a46d1edd4f47
ElodieENSTA <elodie.morin@ensta-bretagne.fr>
2024-07-11 10:45:57 +0200

    Add logs

--- frontend/src/view/audio-annotator/audio-annotator.page.tsx
+++ frontend/src/view/audio-annotator/audio-annotator.page.tsx
@@ -264,10 +264,12 @@ export const AudioAnnotator: React.FC = () => {
 
   console.debug('>>> [Audio annotator render]', isLoading)
   if (isLoading) return <p>Loading...</p>;
-  else if (error) return <p>Error while loading task: <code>{ error }</code></p>
+  console.debug('>>> [Audio annotator render] after isLoading', error, task?.id)
+  if (error) return <p>Error while loading task: <code>{ error }</code></p>
   else if (!task?.id) return <p>Unknown error while loading task.</p>
 
   // Rendering
+  console.debug('>>> [Audio annotator render] will real render')
   return (
     <div id="aplose-annotator" className="annotator container-fluid ps-0">
 
