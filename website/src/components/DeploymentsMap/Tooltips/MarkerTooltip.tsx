54a497743d67909278c2d894e57d2a68429f53a7
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2024-09-13 11:17:10 +0200

    [WD-6] Add provider and dates to marker tooltip

--- website/src/components/DeploymentsMap/Tooltips/MarkerTooltip.tsx
+++ website/src/components/DeploymentsMap/Tooltips/MarkerTooltip.tsx
@@ -9,6 +9,9 @@ export const MarkerTooltip: React.FC<{ deployment: DeploymentAPI }> = ({ deploym
       { deployment.site && <p><small>Site:</small> { deployment.site.name }</p> }
       { deployment.campaign && <p><small>Campaign:</small> { deployment.campaign.name }</p> }
       <p><small>Deployment:</small> { deployment.name }</p>
+      { deployment.provider && <p><small>Provider:</small> { deployment.provider.name }</p> }
+      { deployment.deployment_date && <p><small>Launch:</small> { deployment.deployment_date }</p> }
+      { deployment.recovery_date && <p><small>Recovery:</small> { deployment.recovery_date }</p> }
     </div>
   )
 }
