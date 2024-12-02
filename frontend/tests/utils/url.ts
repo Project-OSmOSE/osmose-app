43c9d5fda87087b96fa89ecf370569977ae9aa4d
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2024-12-02 14:37:28 +0100

    FIX test request url

--- frontend/tests/utils/url.ts
+++ frontend/tests/utils/url.ts
@@ -2,8 +2,8 @@ import { Page } from '@playwright/test';
 
 export const FILE_RANGE_URL = /.*\/api\/annotation-file-range\/?\??.*$/g
 export const CAMPAIGN_URL = /.*\/api\/annotation-campaign\/?\??.*$/g
-export const CREATE_CAMPAIGN_URL = '*/**/api/annotation-campaign/'
-export const DATASET_URL = '*/**/api/dataset'
+export const CREATE_CAMPAIGN_URL = '/api/annotation-campaign/'
+export const DATASET_URL = '/api/dataset/'
 export const LABEL_SET_URL = /api\/label-set\/?$/g
 export const CONFIDENCE_SET_URL = /api\/confidence-indicator\/?$/g
 
