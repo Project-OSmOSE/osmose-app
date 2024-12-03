b94a37b1f861c8b446e8bda8e61895657e62656b
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2024-12-03 14:55:10 +0100

    Review login

--- frontend/src/view/login.page.tsx
+++ frontend/src/view/login.page.tsx
@@ -1,52 +1,44 @@
-import React, { FormEvent, useEffect, useRef, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { IonButton } from "@ionic/react";
-import { OldInput, InputValue } from "@/components/form/inputs/input.tsx";
+import { Input } from "@/components/form/inputs/input.tsx";
 import { OsmoseBarComponent } from "@/view/global-components/osmose-bar/osmose-bar.component.tsx";
-import { InputRef } from "@/components/form/inputs/utils.ts";
-import { useLoginMutation, selectIsConnected } from '@/service/auth';
+import { selectIsConnected, useLoginMutation } from '@/service/auth';
 import { useAppSelector } from '@/slices/app.ts';
 import { getErrorMessage } from '@/service/function.ts';
 
 
 export const Login: React.FC = () => {
-  const history = useHistory();
-  const location = useLocation<any>();
-  const { from } = location.state || { from: { pathname: '/annotation-campaign' } };
-  const inputsRef = useRef<{ [key in 'username' | 'password']: InputRef<InputValue> | null }>({
-    username: null,
-    password: null
-  });
 
   // State
   const isConnected = useAppSelector(selectIsConnected);
-  const [ errorMessage, setErrorMessage ] = useState<string | undefined>();
+  const [ username, setUsername ] = useState<string | undefined>();
+  const [ password, setPassword ] = useState<string | undefined>();
+  const [ errors, setErrors ] = useState<{ global?: string, username?: string, password?: string }>({});
+
+  // Service
+  const history = useHistory();
+  const location = useLocation<any>();
+  const { from } = location.state || { from: { pathname: '/annotation-campaign' } };
 
   // Service
   const [ login, { isLoading } ] = useLoginMutation();
 
   useEffect(() => {
     if (isConnected) history.replace(from);
-  }, [isConnected]);
+  }, [ isConnected ]);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const username = inputsRef.current.username?.validate()?.toString().trim();
-    if (!username) {
-      inputsRef.current.username?.setError("This field is required.")
-    }
-    const password = inputsRef.current.password?.validate()?.toString().trim();
-    if (!username) {
-      inputsRef.current.username?.setError("This field is required.")
-    }
+    setErrors({})
+    if (!username) setErrors({ username: "This field is required." })
+    if (!password) setErrors(prev => ({ ...prev, password: "This field is required." }))
     if (!username || !password) return;
 
     await login({ username, password })
       .unwrap()
       .then(() => history.replace(from))
-      .catch(error => {
-        setErrorMessage(getErrorMessage(error))
-      });
+      .catch(error => setErrors({ global: getErrorMessage(error) }));
   }
 
   return (
@@ -54,17 +46,26 @@ export const Login: React.FC = () => {
       <div className="row text-left h-100 main">
         <div className="col-sm-12 border rounded">
           <h1 className="text-center">Login</h1>
-          { errorMessage && <p className="error-message">{ errorMessage }</p> }
+          { errors.global && <p className="error-message">{ errors.global }</p> }
           <form onSubmit={ handleSubmit }>
             <div className="form-group">
-              <OldInput id="loginInput" className="form-control"
-                        ref={ el => inputsRef.current.username = el }
-                        placeholder="username" label="Login" autoComplete="username"/>
+              <Input id="loginInput"
+                     className="form-control"
+                     value={ username }
+                     onChange={ (e) => setUsername(e.target.value) }
+                     error={ errors.username }
+                     placeholder="username" label="Login" autoComplete="username"/>
             </div>
             <div className="form-group">
-              <OldInput id="passwordInput" className="form-control" ref={ el => inputsRef.current.password = el }
-                        placeholder="password" label="Password"
-                        type="password" autoComplete="current-password"/>
+              <Input id="passwordInput"
+                     className="form-control"
+                     value={ password }
+                     onChange={ e => setPassword(e.target.value) }
+                     error={ errors.password }
+                     placeholder="password"
+                     label="Password"
+                     type="password"
+                     autoComplete="current-password"/>
             </div>
 
             <IonButton color={ "primary" }
