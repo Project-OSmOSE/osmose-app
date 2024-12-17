ed134c6aedcf272be590959df61bc6c4ef0f40e1
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2024-12-17 10:04:23 +0100

    FIX spectrogram images loading

--- frontend/src/services/utils/format.tsx
+++ frontend/src/services/utils/format.tsx
@@ -2,33 +2,35 @@ import { CSVColumns } from "@/types/csv-import-annotations.ts";
 
 
 export function buildErrorMessage(e: any): string {
-  if (e !== null && typeof e === 'object' && e.status && (e.message || e.response)) {
-    if (e.response) {
-      if (e.response.body) {
-        const message = Object
-          .entries(e.response.body)
-          .map(([key, value]) => Array.isArray(value) ? `${ key }: ${ value.join(' - ') }` : '')
-          .join("\n");
-        if (message.length > 0) return message;
-        try {
-          return JSON.stringify(e.response.body)
-        } catch (e) {
-          console.warn(e)
+  if (e !== null && typeof e === 'object') {
+    if (e.status && (e.message || e.response)) {
+      if (e.response) {
+        if (e.response.body) {
+          const message = Object
+            .entries(e.response.body)
+            .map(([ key, value ]) => Array.isArray(value) ? `${ key }: ${ value.join(' - ') }` : '')
+            .join("\n");
+          if (message.length > 0) return message;
+          try {
+            return JSON.stringify(e.response.body)
+          } catch (e) {
+            console.warn(e)
+          }
+        }
+        if (e.response.text) {
+          try {
+            return JSON.stringify(e.response.text)
+          } catch (e) {
+            console.warn(e)
+          }
+          return e.response.text
         }
       }
-      if (e.response.text) {
-        try {
-          return JSON.stringify(e.response.text)
-        } catch (e) {
-          console.warn(e)
-        }
-        return e.response.text
-      }
-    }
-    return 'Status: ' + e.status.toString() +
-      ' - Reason: ' + e.message +
-      (e.response.body?.title ? ` - ${ e.response.body.title }` : '') +
-      (e.response.body?.detail ? ` - ${ e.response.body.detail }` : '');
+      return 'Status: ' + e.status.toString() +
+        ' - Reason: ' + e.message +
+        (e.response.body?.title ? ` - ${ e.response.body.title }` : '') +
+        (e.response.body?.detail ? ` - ${ e.response.body.detail }` : '');
+    } else return JSON.stringify(e);
   } else if (typeof e === 'string') {
     return e;
   } else {
@@ -39,7 +41,7 @@ export function buildErrorMessage(e: any): string {
 export function formatCSV(content: string,
                           separator: string,
                           columns: CSVColumns) {
-  let lines = content.replace('\r', '').split('\n').map(l => [l]);
+  let lines = content.replace('\r', '').split('\n').map(l => [ l ]);
   lines = lines.map(l => l.flatMap(l => l.split(separator)));
 
   const headers = lines.shift()?.map(h => h.replace('\r', ''));
