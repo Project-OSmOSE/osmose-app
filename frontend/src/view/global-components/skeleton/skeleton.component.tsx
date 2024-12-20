97b8723351507dbac119b0a166c8ec4063e772c9
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2024-12-20 09:15:29 +0100

    New skeleton

--- frontend/src/view/global-components/skeleton/skeleton.component.tsx
+++ frontend/src/view/global-components/skeleton/skeleton.component.tsx
@@ -2,11 +2,12 @@ import React, { ReactNode } from 'react';
 import { OsmoseBarComponent } from "@/view/global-components/osmose-bar/osmose-bar.component.tsx";
 import { Link } from 'react-router-dom';
 import { IonButton, IonIcon } from "@ionic/react";
-import './skeleton.component.css';
+import styles from './skeleton.module.scss';
 import { openOutline } from 'ionicons/icons';
 import { useAppDispatch } from '@/service/app';
 import { logout } from '@/service/auth';
 import { useGetCurrentUserQuery } from '@/service/user';
+import logo from '/app/images/ode_logo_192x192.png';
 
 export const AploseSkeleton: React.FC<{ children?: ReactNode }> = ({ children }) => {
 
@@ -15,22 +16,31 @@ export const AploseSkeleton: React.FC<{ children?: ReactNode }> = ({ children })
   const { data: currentUser } = useGetCurrentUserQuery();
 
   return (
-    <div id="aplose-skeleton-component">
-      <h1>APLOSE</h1>
+    <div className={ styles.page }>
 
-      <div id="nav" className="border rounded">
-        <a href="/app/">Back to Home</a>
-        { currentUser?.is_staff && <Link to="/datasets">Datasets</Link> }
-        <Link to="/annotation-campaign">Annotation campaigns</Link>
-        { currentUser?.is_staff && <a href="/backend/admin" target="_blank">Admin <IonIcon icon={ openOutline }/></a> }
-        <br/>
+      <div className={ styles.nav }>
+
+        <div className={ styles.title }>
+          <img src={ logo } alt="APLOSE"/>
+          <h1>APLOSE</h1>
+        </div>
+
+        <div className={ styles.links }>
+          <a href="/app/">Back to Home</a>
+          <Link to="/annotation-campaign">Annotation campaigns</Link>
+          { currentUser?.is_staff && <Link to="/datasets">Datasets</Link> }
+          { currentUser?.is_staff &&
+              <a href="/backend/admin" target="_blank">Admin <IonIcon icon={ openOutline }/></a> }
+
+        </div>
+
         <IonButton color={ "medium" }
                    onClick={ () => dispatch(logout()) }>
           Logout
         </IonButton>
       </div>
 
-      <div id="content" className="border rounded">
+      <div>
         { children }
       </div>
 
