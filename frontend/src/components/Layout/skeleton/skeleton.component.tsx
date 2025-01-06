63329027b7c56a12b55c8791561bd83fd2ffdac9
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-01-06 16:31:04 +0100

    Update skeleton to better place IonSpinner

--- frontend/src/components/Layout/skeleton/skeleton.component.tsx
+++ frontend/src/components/Layout/skeleton/skeleton.component.tsx
@@ -11,7 +11,7 @@ export const AploseSkeleton: React.FC<{ children?: ReactNode }> = ({ children })
 
       <Navbar/>
 
-      <div>
+      <div style={{ display: 'grid', alignItems: 'start'}}>
         { children }
       </div>
 
