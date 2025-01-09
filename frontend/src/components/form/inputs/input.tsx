83a03f2d0e7000d37261e2ed2cfb07442df2a045
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-01-09 15:35:45 +0100

    Add view feature for password input

--- frontend/src/components/form/inputs/input.tsx
+++ frontend/src/components/form/inputs/input.tsx
@@ -1,7 +1,7 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { Fragment, HTMLInputTypeAttribute, InputHTMLAttributes, useState } from "react";
 import { IonIcon, IonNote } from "@ionic/react";
 import './inputs.css';
-
+import { eyeOffOutline, eyeOutline } from "ionicons/icons";
 
 type InputProperties = {
   label?: string;
@@ -19,9 +19,18 @@ export const Input: React.FC<InputProperties> = ({
                                                    value,
                                                    ...inputArgs
                                                  }) => {
-
   const className = [];
   if (startIcon) className.push('has-start-icon');
+  if (inputArgs.type === 'password') className.push('has-end-icon');
+
+  const [ type, setType ] = useState<HTMLInputTypeAttribute | undefined>(inputArgs.type);
+
+  function toggleType() {
+    if (inputArgs.type !== 'password') return;
+    if (type === 'password') setType('text');
+    else setType('password');
+  }
+
   return <div id="aplose-input" aria-disabled={ disabled } aria-invalid={ !!error }>
     { label && <label id="label"
                       className={ required ? 'required' : '' }>
@@ -31,10 +40,16 @@ export const Input: React.FC<InputProperties> = ({
     <div id="input">
       { startIcon && <IonIcon id="input-start-icon" icon={ startIcon }/> }
       <input { ...inputArgs }
+             type={ type }
              value={ value }
              required={ required }
              disabled={ disabled }
              className={ `${ className.join(' ') } ${ inputArgs.className }` }/>
+
+      { inputArgs.type === 'password' && <Fragment>
+        { type === 'password' && <IonIcon id='input-end-icon' icon={ eyeOutline } onClick={ toggleType }/> }
+        { type === 'text' && <IonIcon id='input-end-icon' icon={ eyeOffOutline } onClick={ toggleType }/> }
+      </Fragment> }
     </div>
 
     { note && <IonNote>{ note }</IonNote> }
