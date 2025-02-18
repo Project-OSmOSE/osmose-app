326124abee9f8d520e0a24a91453612ab97c131d
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-02-18 09:46:24 +0100

    Display of footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import logo from "/images/ode_logo_192x192.png";
 import { CONTACT_MAIL, CONTACT_URI, GITHUB_URL, OSMOSE_URL } from "@/consts/links.ts";
 import { Link } from "@/components/ui";
@@ -7,24 +7,21 @@ import { logoGithub, mailOutline } from "ionicons/icons";
 import style from './layout.module.scss';
 
 export const Footer: React.FC = () => {
-  const [ showVersion, setShowVersion ] = useState<boolean>(false);
   const version = useMemo(() => import.meta.env.VITE_GIT_TAG, [])
-
-  function toggleVersion() {
-    setShowVersion(!showVersion);
-  }
 
   return (
     <footer className={ style.footer }>
-      <Link href={ GITHUB_URL } target='_blank' color='medium'>
-        <IonIcon icon={ logoGithub } slot='start'/>
-        Github
-      </Link>
+      <div>
+        <Link href={ GITHUB_URL } target='_blank' color='medium'>
+          <IonIcon icon={ logoGithub } slot='start'/>
+          Github
+        </Link>
+        <IonNote color='medium'>{ version }</IonNote>
+      </div>
 
-      <div className={ style.proposition } onClick={ toggleVersion }>
+      <div className={ style.proposition }>
         <p>Proposed by</p>
         <Link href={ OSMOSE_URL } onClick={ e => e.stopPropagation() }>OSmOSE <img src={ logo } alt="OSmOSE"/></Link>
-        { showVersion && <IonNote color='medium'>{ version }</IonNote> }
       </div>
 
       <Link href={ CONTACT_URI } color='medium'>
