93783aa372aa2e4431753cb56b12fb1437f00010
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-03-13 11:25:14 +0100

    [ADEV-213] When changing zoom of spectrogram, annotations can take times to adjust their position

--- frontend/src/view/annotator/tools/spectrogram/annotation/Box.tsx
+++ frontend/src/view/annotator/tools/spectrogram/annotation/Box.tsx
@@ -72,10 +72,6 @@ export const Box: React.FC<RegionProps> = ({
   useEffect(() => updateWidth, [ _xAxis.current, _start_time.current, _end_time.current ]);
   useEffect(() => updateTop, [ _yAxis.current, _end_frequency.current ]);
   useEffect(() => updateHeight, [ _yAxis.current, _start_frequency.current, _end_frequency.current ]);
-  useEffect(() => setTop(_top.current), [ _top.current ]);
-  useEffect(() => setHeight(_height.current), [ _height.current ]);
-  useEffect(() => setLeft(_left.current), [ _left.current ]);
-  useEffect(() => setWidth(_width.current), [ _width.current ]);
 
   // Memo
   const colorClassName: string = useMemo(() => label_set ? `ion-color-${ label_set.labels.indexOf(annotation.label) }` : '', [ label_set, annotation.label ]);
@@ -84,37 +80,45 @@ export const Box: React.FC<RegionProps> = ({
   function updateLeft() {
     if (_start_time.current === null) return;
     _left.current = _xAxis.current.valueToPosition(_start_time.current);
+    setLeft(_left.current)
   }
 
   function updateTop() {
     if (_end_frequency.current === null) return;
     _top.current = _yAxis.current.valueToPosition(_end_frequency.current);
+    setTop(_top.current)
   }
 
   function updateWidth() {
     if (_start_time.current === null || _end_time.current === null) return;
     _width.current = _xAxis.current.valuesToPositionRange(_start_time.current, _end_time.current);
+    setWidth(_width.current)
   }
 
   function updateHeight() {
     if (_start_frequency.current === null || _end_frequency.current === null) return;
     _height.current = _yAxis.current.valuesToPositionRange(_start_frequency.current, _end_frequency.current);
+    setHeight(_height.current)
   }
 
   function onTopMove(movement: number) {
     _top.current += movement;
+    setTop(_top.current)
   }
 
   function onHeightMove(movement: number) {
     _height.current += movement;
+    setHeight(_height.current)
   }
 
   function onLeftMove(movement: number) {
     _left.current += movement;
+    setLeft(_left.current)
   }
 
   function onWidthMove(movement: number) {
     _width.current += movement;
+    setWidth(_width.current)
   }
 
   function onValidateMove() {
