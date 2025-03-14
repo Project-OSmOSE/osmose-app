db7cfb8f61f618530d310ba80fbea99cd51f6d51
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-03-14 12:10:30 +0100

    [ADEV-204] Add tests for annotations import

--- frontend/tests/08_Campaign_import_annotations.spec.ts
+++ frontend/tests/08_Campaign_import_annotations.spec.ts
@@ -1,83 +1,68 @@
 import { API_URL, ESSENTIAL, expect, test } from "./utils";
-import { WriteCheckAnnotationCampaign } from "../src/service/campaign";
-import { CAMPAIGN, DATASET, DETECTOR_CONFIGURATION, LABEL } from "./fixtures";
+import { DATASET, DETECTOR_CONFIGURATION } from "./fixtures";
 
 
-test('[Check] Can create a campaign', ESSENTIAL, async ({ page }) => {
-  await page.campaign.create.go('annotator');
-  await page.campaign.create.fillGlobal({ complete: true });
-  await page.campaign.create.fillData();
-  await page.campaign.create.fillAnnotationCheck();
-  await page.campaign.create.fillAcousticFeatures();
+test.describe('Campaign creator', () => {
+  test('[Check] Can import all', ESSENTIAL, async ({ page }) => {
+    await page.campaign.create.go('annotator');
+    await page.campaign.create.fillGlobal()
+    await page.campaign.create.fillData()
+    await page.campaign.create.selectMode('Check annotations')
+    await page.mock.detectors(true)
+    await page.mock.resultImport()
+    await page.mock.campaignDetail(false, "Check")
+    await page.campaign.create.createButton.click()
+    await expect(page.getByRole('heading', { name: "Import annotations" })).toBeVisible()
 
-  const [
-    submitResultsRequest,
-    submittedCampaign
-  ] = await Promise.all([
-    page.waitForRequest(API_URL.result.import),
-    page.waitForRequest(API_URL.campaign.create),
-    page.campaign.create.createButton.click()
-  ])
-  const expectedCampaign: WriteCheckAnnotationCampaign = {
-    name: CAMPAIGN.name,
-    instructions_url: CAMPAIGN.instructions_url,
-    desc: CAMPAIGN.desc,
-    deadline: CAMPAIGN.deadline,
-    datasets: [ DATASET.name ],
-    spectro_configs: DATASET.spectros.map(s => s.id),
-    labels_with_acoustic_features: [ LABEL.withFeatures ],
-    usage: 'Check',
-    allow_point_annotation: false,
-  }
-  expect(await submittedCampaign.postDataJSON()).toEqual(expectedCampaign);
-  expect(submitResultsRequest.url()).toContain(`dataset_name=${ encodeURI(DATASET.name) }`)
-  expect(submitResultsRequest.url()).toContain(`detectors_map=${ encodeURI(JSON.stringify({
-    detector1: { detector: 'detector1', configuration: DETECTOR_CONFIGURATION },
-    detector3: { detector: 'detector3', configuration: DETECTOR_CONFIGURATION },
-    detector2: { detector: 'detector2', configuration: DETECTOR_CONFIGURATION },
-  })) }`)
-});
+    await page.campaign.import.fillAnnotationCheck();
 
-test('[Check] Can import only first Dataset x Detector', ESSENTIAL, async ({ page }) => {
-  await page.campaign.create.go('annotator')
-  await page.campaign.create.fillData()
-  await page.campaign.create.fillAnnotationCheck({ onlyFirstDataset: true, onlyFirstDetector: true })
+    const [
+      submitResultsRequest,
+    ] = await Promise.all([
+      page.waitForRequest(API_URL.result.import),
+      page.campaign.import.importButton.click()
+    ])
+    expect(submitResultsRequest.url()).toContain(`dataset_name=${ encodeURI(DATASET.name) }`)
+    expect(submitResultsRequest.url()).toContain(`detectors_map=${ encodeURI(JSON.stringify({
+      detector1: { detector: 'detector1', configuration: DETECTOR_CONFIGURATION },
+      detector2: { detector: 'detector2', configuration: DETECTOR_CONFIGURATION },
+      detector3: { detector: 'detector3', configuration: DETECTOR_CONFIGURATION },
+    })) }`)
 
-  //TODO: Filter labels shown with the dataset and detectors filtering
-  // await expect(page.getByText(Mock.CLASSIC_LABEL, { exact: true })).toBeVisible();
-  // await expect(page.getByText(Mock.FEATURE_LABEL, { exact: true })).not.toBeVisible();
-  await expect(page.getByText('detector1', { exact: true })).toBeVisible();
-  await expect(page.getByText('detector2', { exact: true })).not.toBeVisible();
-  await expect(page.getByText('detector3', { exact: true })).not.toBeVisible();
-});
+    await expect(page.getByRole('heading', { name: "Manage annotators" })).toBeVisible()
+  });
 
-test('[Check] Can handle existing detector', ESSENTIAL, async ({ page }) => {
-  await page.campaign.create.go('annotator', { loadDetectors: true })
-  await page.campaign.create.fillData()
-  await page.campaign.create.selectMode('Check annotations')
-  const modal = await page.campaign.create.openImportModal()
-  await page.campaign.create.importFile(modal);
-  await page.campaign.create.selectDataset(modal, { onlyFirstDataset: true, onlyFirstDetector: true })
+  test('[Check] Can import only first Detector', ESSENTIAL, async ({ page }) => {
+    await page.campaign.import.go('creator');
+    await page.campaign.import.fillAnnotationCheck({ onlyFirstDetector: true });
 
-  await test.step('Select Detectors', async () => {
-    await expect(modal.getByText('detector1Already in database').first()).toBeVisible()
-    await modal.getByRole('button', { name: `Confirm` }).click({ force: true });
-  })
+    await expect(page.getByText('detector1', { exact: true })).toBeVisible();
+    await expect(page.getByText('detector2', { exact: true })).not.toBeVisible();
+    await expect(page.getByText('detector3', { exact: true })).not.toBeVisible();
+  });
 
-  await test.step('Select Detectors configurations', async () => {
-    await modal.getByText('Select configuration').first().click()
-    await expect(modal.getByText(DETECTOR_CONFIGURATION).and(modal.locator('div'))).toBeVisible()
-  })
-});
+  test('[Check] Can handle existing detector', ESSENTIAL, async ({ page }) => {
+    await page.campaign.import.go('creator', { loadDetectors: true });
+    await page.campaign.import.importFile();
 
-test('[Check] Can delete import', async ({ page }) => {
-  await page.campaign.create.go('annotator')
-  await page.campaign.create.fillData()
-  await page.campaign.create.fillAnnotationCheck()
+    await test.step('Select Detectors', async () => {
+      await expect(page.getByText('detector1Already in database').first()).toBeVisible()
+    })
 
-  await test.step('Remove detectors', async () => {
-    await page.locator('#detector-import-results').getByRole('button').click()
-  })
+    await test.step('Select Detectors configurations', async () => {
+      await page.getByText('Select configuration').first().click()
+      await expect(page.getByText(DETECTOR_CONFIGURATION).and(page.locator('div'))).toBeVisible()
+    })
+  });
 
-  await expect(page.getByRole('button', { name: 'Import annotations' })).toBeVisible();
+  test('[Check] Can reset import', async ({ page }) => {
+    await page.campaign.import.go('creator')
+    await page.campaign.import.fillAnnotationCheck()
+
+    await test.step('Reset file', async () => {
+      await page.campaign.import.resetFileButton.click()
+    })
+
+    await expect(page.getByText('Import annotations (csv)')).toBeVisible();
+  });
 });
