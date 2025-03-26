7b44a23d9d0d3e4b33d04ba54da07212da3258f3
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-03-26 10:06:22 +0100

    [ADEV-208] Update tests

--- frontend/tests/01_Login.spec.ts
+++ frontend/tests/01_Login.spec.ts
@@ -2,21 +2,31 @@ import { ESSENTIAL, expect, test } from './utils';
 import { LoginPage } from './utils/pages';
 import { AUTH } from './fixtures';
 
-test('Login', ESSENTIAL, async ({ page }) => {
+test('Login - Unauthorized', ESSENTIAL, async ({ page }) => {
   await page.login.go();
   await page.login.fillForm();
+  await page.login.submit({ status: 401, submitAction: 'button' });
+  await expect(page.getByText(LoginPage.SERVER_ERROR)).toBeVisible();
+})
 
-  await test.step('401 - Unauthorized', async () => {
-    await page.login.submit(401);
-    await expect(page.getByText(LoginPage.SERVER_ERROR)).toBeVisible();
+test('Login - Success', ESSENTIAL, async ({ page }) => {
+  await page.login.go();
+  await page.login.fillForm();
+  const request = await page.login.submit({ status: 200, submitAction: 'button' });
+  expect(await request.postDataJSON()).toEqual({
+    username: AUTH.username,
+    password: AUTH.password
   })
+  await expect(page.getByRole('heading', { name: 'Annotation Campaigns' })).toBeVisible();
+})
 
-  await test.step('200 - Success', async () => {
-    const request = await page.login.submit(200);
-    expect(await request.postDataJSON()).toEqual({
-      username: AUTH.username,
-      password: AUTH.password
-    })
-    await expect(page.getByRole('heading', { name: 'Annotation Campaigns' })).toBeVisible();
+test('Login - Success with Enter key', ESSENTIAL, async ({ page }) => {
+  await page.login.go();
+  await page.login.fillForm();
+  const request = await page.login.submit({ status: 200, submitAction: 'enterKey' });
+  expect(await request.postDataJSON()).toEqual({
+    username: AUTH.username,
+    password: AUTH.password
   })
+  await expect(page.getByRole('heading', { name: 'Annotation Campaigns' })).toBeVisible();
 })
