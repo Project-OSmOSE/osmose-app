749a8eab54f2cfb14de9a2a91d099770a3ae7c49
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-03-27 09:56:00 +0100

    [ADEV-204] Change replace file option to reset import

---
+++ frontend/src/service/campaign/result/import/hook.ts
@@ -0,0 +1,114 @@
+import { useParams } from "react-router-dom";
+import { CampaignAPI } from "@/service/campaign";
+import { AnnotationResultAPI } from "@/service/campaign/result";
+import { useAppDispatch, useAppSelector } from "@/service/app.ts";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { CHUNK_SIZE } from "@/service/campaign/result/import/type.ts";
+import { getErrorMessage } from "@/service/function.ts";
+import { QueryStatus } from "@reduxjs/toolkit/query";
+import { ResultImportSlice } from "@/service/campaign/result/import/slice.ts";
+
+export const useUploadResultChunk = (onFulfilled: () => void) => {
+  const { id: campaignID } = useParams<{ id: string }>();
+  const { data: campaign } = CampaignAPI.useRetrieveQuery(campaignID);
+  const [ doImport, {
+    error,
+    originalArgs,
+    reset,
+    fulfilledTimeStamp,
+    startedTimeStamp,
+    status
+  } ] = AnnotationResultAPI.useImportMutation()
+  const { file, detectors, upload: uploadInfo } = useAppSelector(state => state.resultImport)
+  const dispatch = useAppDispatch()
+  const [ force, setForce ] = useState<boolean>(false);
+
+  const rows = useMemo(() => {
+    if (file.state === 'loaded')
+      return file.rows.filter(r => r.some(cell => detectors.selection.includes(cell)));
+    return [];
+  }, [ file, detectors.selection ])
+
+  const detectorsMap = useMemo(() => {
+    const detectors_map: { [key in string]: { detector: string, configuration: string } } = {}
+    for (const d of detectors.selection) {
+      const detector = detectors.mapToKnown[d]?.name ?? d;
+      const config = detectors.mapToConfiguration[detector];
+      detectors_map[d] = {
+        detector,
+        configuration: typeof config === 'string' ? config : config?.configuration ?? '',
+      }
+    }
+    return detectors_map
+  }, [ detectors ])
+
+  const uploadChunk = useCallback(() => {
+    if (!campaign) return;
+    if (file.state !== 'loaded') return;
+    if (uploadInfo.state !== 'uploading') return;
+    doImport({
+      campaignID: campaign.id,
+      datasetName: campaign.datasets![0],
+      detectors_map: detectorsMap,
+      data: [ file.header, ...rows.slice(uploadInfo.uploaded + 1, uploadInfo.uploaded + 1 + CHUNK_SIZE) ],
+      force
+    })
+  }, [ doImport, detectorsMap ])
+
+  const upload = useCallback((force?: boolean) => {
+    if (file.state !== 'loaded') return;
+    if (uploadInfo.state === 'uploading') return;
+    if (force !== undefined) setForce(force);
+    dispatch(ResultImportSlice.actions.updateUpload({
+      state: 'uploading',
+      uploaded: uploadInfo.state === 'initial' ? 0 : uploadInfo.uploaded,
+      total: rows.length,
+      duration: uploadInfo.state === 'initial' ? 0 : uploadInfo.duration,
+      remainingDurationEstimation: undefined
+    }))
+    uploadChunk()
+  }, [ uploadInfo, rows, detectorsMap ])
+
+  // Update duration and remainingDurationEstimation
+  useEffect(() => {
+    console.log('TIMESTAMP >>', startedTimeStamp, fulfilledTimeStamp)
+    if (uploadInfo.state !== 'uploading') return;
+    if (!fulfilledTimeStamp || !startedTimeStamp) return;
+    const duration = uploadInfo.duration + (fulfilledTimeStamp - startedTimeStamp);
+    dispatch(ResultImportSlice.actions.updateUpload({
+      ...uploadInfo,
+      duration,
+      remainingDurationEstimation: (uploadInfo.total - uploadInfo.uploaded) * duration / uploadInfo.uploaded,
+    }))
+  }, [ startedTimeStamp, fulfilledTimeStamp ]);
+
+  // Manage chunk import failed
+  useEffect(() => {
+    if (uploadInfo.state !== 'uploading') return;
+    if (error) dispatch(ResultImportSlice.actions.updateUpload({
+      ...uploadInfo,
+      state: 'error',
+      error: getErrorMessage(error) ?? 'Unknown error',
+      canForce: (error as any).canForce
+    }))
+  }, [ error ]);
+
+  // Manage chunk import fulfilled
+  useEffect(() => {
+    if (uploadInfo.state !== 'uploading') return;
+    if (status !== QueryStatus.fulfilled) return;
+    if (!originalArgs) return;
+    const uploaded = uploadInfo.uploaded + originalArgs.data.length - 1; // Don't count headers
+    const state = uploadInfo.total > uploaded ? 'uploading' : 'fulfilled';
+    dispatch(ResultImportSlice.actions.updateUpload({
+      ...uploadInfo,
+      uploaded,
+      state,
+    }))
+    reset();
+    if (state === 'uploading') uploadChunk();
+    if (state === 'fulfilled') onFulfilled();
+  }, [ status, originalArgs ]);
+
+  return { upload }
+}
--- frontend/src/service/campaign/result/import/slice.ts
+++ frontend/src/service/campaign/result/import/slice.ts
@@ -1,22 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import {
-  CHUNK_SIZE,
-  EmptyCSVError,
-  ImportSliceState,
-  UnreadableFileError,
-  UnsupportedCSVError,
-  WrongMIMETypeError
-} from "./type";
+import { EmptyCSVError, ImportSliceState, UnreadableFileError, UnsupportedCSVError, WrongMIMETypeError } from "./type";
 import { ACCEPT_CSV_MIME_TYPE, ACCEPT_CSV_SEPARATOR, IMPORT_ANNOTATIONS_COLUMNS } from "@/consts/csv.ts";
 import { formatCSVToTable } from "@/services/utils/format.tsx";
 import { Detector, DetectorConfiguration } from "@/service/campaign/detector";
-import { AnnotationResultAPI } from "@/service/campaign/result";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/service/app.ts";
-import { useParams } from "react-router-dom";
-import { CampaignAPI } from "@/service/campaign";
-import { getErrorMessage } from "@/service/function.ts";
-import { QueryStatus } from "@reduxjs/toolkit/query";
 
 export const loadFile = createAsyncThunk(
   'campaign/loadFile',
@@ -92,6 +78,11 @@ export const ResultImportSlice = createSlice({
     },
     updateUpload: (state, action: { payload: ImportSliceState['upload'] }) => {
       state.upload = action.payload;
+    },
+    updateFile: (state) => {
+      if (state.upload.state === 'initial') return;
+      state.upload.state = 'update file';
+      state.file = { state: 'initial' };
     }
   },
   extraReducers: builder => {
@@ -115,107 +106,3 @@ export const ResultImportSlice = createSlice({
     })
   }
 })
-
-export const useUploadResultChunk = (onFulfilled: () => void) => {
-  const { id: campaignID } = useParams<{ id: string }>();
-  const { data: campaign } = CampaignAPI.useRetrieveQuery(campaignID);
-  const [ doImport, {
-    error,
-    originalArgs,
-    reset,
-    fulfilledTimeStamp,
-    startedTimeStamp,
-    status
-  } ] = AnnotationResultAPI.useImportMutation()
-  const { file, detectors, upload: uploadInfo } = useAppSelector(state => state.resultImport)
-  const dispatch = useAppDispatch()
-  const [ force, setForce ] = useState<boolean>(false);
-
-  const rows = useMemo(() => {
-    if (file.state === 'loaded')
-      return file.rows.filter(r => r.some(cell => detectors.selection.includes(cell)));
-    return [];
-  }, [ file, detectors.selection ])
-
-  const detectorsMap = useMemo(() => {
-    const detectors_map: { [key in string]: { detector: string, configuration: string } } = {}
-    for (const d of detectors.selection) {
-      const detector = detectors.mapToKnown[d]?.name ?? d;
-      const config = detectors.mapToConfiguration[detector];
-      detectors_map[d] = {
-        detector,
-        configuration: typeof config === 'string' ? config : config?.configuration ?? '',
-      }
-    }
-    return detectors_map
-  }, [ detectors ])
-
-  const uploadChunk = useCallback(() => {
-    if (!campaign) return;
-    if (file.state !== 'loaded') return;
-    if (uploadInfo.state !== 'uploading') return;
-    doImport({
-      campaignID: campaign.id,
-      datasetName: campaign.datasets![0],
-      detectors_map: detectorsMap,
-      data: [ file.header, ...rows.slice(uploadInfo.uploaded + 1, uploadInfo.uploaded + 1 + CHUNK_SIZE) ],
-      force
-    })
-  }, [ doImport, detectorsMap ])
-
-  const upload = useCallback((force?: boolean) => {
-    if (file.state !== 'loaded') return;
-    if (uploadInfo.state === 'uploading') return;
-    if (force !== undefined) setForce(force);
-    dispatch(ResultImportSlice.actions.updateUpload({
-      state: 'uploading',
-      uploaded: uploadInfo.state === 'initial' ? 0 : uploadInfo.uploaded,
-      total: rows.length,
-      duration: uploadInfo.state === 'initial' ? 0 : uploadInfo.duration,
-      remainingDurationEstimation: undefined
-    }))
-    uploadChunk()
-  }, [ uploadInfo, rows, detectorsMap ])
-
-  // Update duration and remainingDurationEstimation
-  useEffect(() => {
-    console.log('TIMESTAMP >>', startedTimeStamp, fulfilledTimeStamp)
-    if (uploadInfo.state !== 'uploading') return;
-    if (!fulfilledTimeStamp || !startedTimeStamp) return;
-    const duration = uploadInfo.duration + (fulfilledTimeStamp - startedTimeStamp);
-    dispatch(ResultImportSlice.actions.updateUpload({
-      ...uploadInfo,
-      duration,
-      remainingDurationEstimation: (uploadInfo.total - uploadInfo.uploaded) * duration / uploadInfo.uploaded,
-    }))
-  }, [ startedTimeStamp, fulfilledTimeStamp ]);
-
-  // Manage chunk import failed
-  useEffect(() => {
-    if (uploadInfo.state !== 'uploading') return;
-    if (error) dispatch(ResultImportSlice.actions.updateUpload({
-      ...uploadInfo,
-      state: 'error',
-      error: getErrorMessage(error) ?? 'Unknown error',
-      canForce: (error as any).canForce
-    }))
-  }, [ error ]);
-
-  // Manage chunk import fulfilled
-  useEffect(() => {
-    if (uploadInfo.state !== 'uploading') return;
-    if (status !== QueryStatus.fulfilled) return;
-    if (!originalArgs) return;
-    const uploaded = uploadInfo.uploaded + originalArgs.data.length - 1; // Don't count headers
-    const state = uploadInfo.total > uploaded ? 'uploading' : 'fulfilled';
-    dispatch(ResultImportSlice.actions.updateUpload({
-      ...uploadInfo,
-      uploaded,
-      state,
-    }))
-    reset();
-    if (state === 'uploading') uploadChunk();
-  }, [ status, originalArgs ]);
-
-  return { upload }
-}
