1716a8cbda2ca6bc8b14b53379180d8d2a93aaad
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-04-14 10:58:14 +0200

    [ADEV-232] Add tooltip hints on annotation header

--- frontend/src/view/annotator/tools/spectrogram/annotation/Headers.tsx
+++ frontend/src/view/annotator/tools/spectrogram/annotation/Headers.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, useCallback } from 'react';
+import React, { Fragment, MutableRefObject, useCallback } from 'react';
 import { ExtendedDiv } from '@/components/ui/ExtendedDiv';
 import styles from './annotation.module.scss';
 import { focusResult, removeResult } from '@/service/annotator';
@@ -7,6 +7,7 @@ import { useAnnotator } from '@/service/annotator/hook.ts';
 import { useAppDispatch } from '@/service/app.ts';
 import { AnnotationResult } from '@/service/campaign/result';
 import { useAudioService } from '@/services/annotator/audio.service.ts';
+import { TooltipOverlay } from "@/components/ui";
 
 export const AnnotationHeader: React.FC<{
   active: boolean;
@@ -21,7 +22,6 @@ export const AnnotationHeader: React.FC<{
 }> = ({ active, top, onTopMove, onLeftMove, setIsMouseHover, onValidateMove, className, annotation, audioPlayer }) => {
   const { campaign } = useAnnotator();
   const dispatch = useAppDispatch();
-  const audioService = useAudioService(audioPlayer);
   const _setIsMouseHover = useCallback((value: boolean) => {
     if (!setIsMouseHover) return;
     setIsMouseHover(value);
@@ -36,19 +36,48 @@ export const AnnotationHeader: React.FC<{
                       innerClassName={ styles.inner }
                       onClick={ () => dispatch(focusResult(annotation.id)) }>
 
-    <IoPlayCircle className={ styles.button } onClick={ () => audioService.play(annotation) }/>
+    <PlayButton annotation={ annotation } audioPlayer={ audioPlayer }/>
+
+    <CommentInfo annotation={annotation}/>
 
     <p>{ annotation.label }</p>
 
-    { annotation.comments.length > 0 ?
-      <IoChatbubbleEllipses/> :
-      <IoChatbubbleOutline className={ styles.outlineIcon }/> }
-
-    {/* 'remove-box' class is for playwright tests*/ }
-    { campaign?.usage === 'Create' && (
-      <IoTrashBin className={ [ styles.button, styles.delete, 'remove-box' ].join(' ') }
-                  onClick={ () => dispatch(removeResult(annotation.id)) }/>
-    ) }
+    <TrashButton annotation={annotation}/>
 
   </ExtendedDiv>
 }
+
+export const PlayButton: React.FC<{
+  audioPlayer: MutableRefObject<HTMLAudioElement | null>;
+  annotation: AnnotationResult;
+}> = ({ annotation, audioPlayer }) => {
+  const audioService = useAudioService(audioPlayer);
+  return (
+    <TooltipOverlay tooltipContent={ <p>Play the audio of the annotation</p> }>
+      <IoPlayCircle className={ styles.button } onClick={ () => audioService.play(annotation) }/>
+    </TooltipOverlay>
+  )
+}
+
+export const CommentInfo: React.FC<{ annotation: AnnotationResult; }> = ({ annotation }) => {
+  if (annotation.comments.length > 0) return <IoChatbubbleEllipses/>
+  else return (
+    <TooltipOverlay tooltipContent={ <p>No comments</p> }><IoChatbubbleOutline className={ styles.outlineIcon }/></TooltipOverlay>
+  )
+}
+
+export const TrashButton: React.FC<{
+  annotation: AnnotationResult;
+}> = ({ annotation }) => {
+  const { campaign } = useAnnotator();
+  const dispatch = useAppDispatch();
+  if (campaign?.usage !== 'Create') return <Fragment/>;
+  return (
+    <TooltipOverlay tooltipContent={ <p>Remove the annotation</p> }>
+      {/* 'remove-box' class is for playwright tests*/ }
+      <IoTrashBin className={ [ styles.button, styles.delete, 'remove-box' ].join(' ') }
+                  onClick={ () => dispatch(removeResult(annotation.id)) }/>
+    </TooltipOverlay>
+  )
+}
+
