d5b73509e1a869c6d725d16da4c55976a694db3b
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-07-16 16:32:00 +0200

    fix old migrations

--- backend/api/schema/__init__.py
+++ backend/api/schema/__init__.py
@@ -2,6 +2,7 @@
 import graphene
 from graphene_django_pagination import DjangoPaginationConnectionField
 
+from .annotation import *
 from .common import *
 from .data import *
 
@@ -20,3 +21,24 @@ class APIQuery(graphene.ObjectType):
     all_multi_linear_scales = DjangoPaginationConnectionField(MultiLinearScaleNode)
     all_spectrograms = DjangoPaginationConnectionField(SpectrogramNode)
     all_spectrogram_analysis = DjangoPaginationConnectionField(SpectrogramAnalysisNode)
+
+    all_acoustic_features = DjangoPaginationConnectionField(AcousticFeaturesNode)
+    all_annotations = DjangoPaginationConnectionField(AnnotationNode)
+    all_annotation_campaigns = DjangoPaginationConnectionField(AnnotationCampaignNode)
+    all_annotation_comments = DjangoPaginationConnectionField(AnnotationCommentNode)
+    all_annotation_file_ranges = DjangoPaginationConnectionField(
+        AnnotationFileRangeNode
+    )
+    all_annotation_phases = DjangoPaginationConnectionField(AnnotationPhaseNode)
+    all_annotation_tasks = DjangoPaginationConnectionField(AnnotationTaskNode)
+    all_annotation_validations = DjangoPaginationConnectionField(
+        AnnotationValidationNode
+    )
+    all_confidences = DjangoPaginationConnectionField(ConfidenceNode)
+    all_confidence_sets = DjangoPaginationConnectionField(ConfidenceSetNode)
+    all_detectors = DjangoPaginationConnectionField(DetectorNode)
+    all_detector_configurations = DjangoPaginationConnectionField(
+        DetectorConfigurationNode
+    )
+    all_labels = DjangoPaginationConnectionField(LabelNode)
+    all_label_sets = DjangoPaginationConnectionField(LabelSetNode)
