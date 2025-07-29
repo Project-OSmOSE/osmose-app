f66f683809e39fadd6e6adf128c0f121c9f3f1bc
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-07-29 10:30:46 +0200

    feat(dataset): update download config to handle new osekit

--- backend/api/view/download.py
+++ backend/api/view/download.py
@@ -33,14 +33,48 @@ class DownloadViewSet(ViewSet):
 
         # Create the zipfile, giving the buffer as the target
         with zipfile.ZipFile(zip_buffer, "w") as zip_file:
-            zip_file.writestr(
-                "audio_metadatum.csv",
-                data=analysis.legacy_audio_metadatum_csv().encode("utf-8"),
-            )
-            zip_file.writestr(
-                "spectrogram_configuration.csv",
-                data=analysis.legacy_spectrogram_configuration_csv().encode("utf-8"),
-            )
+            if analysis.legacy:
+                zip_file.writestr(
+                    "audio_metadatum.csv",
+                    data=analysis.legacy_audio_metadatum_csv().encode("utf-8"),
+                )
+                zip_file.writestr(
+                    "spectrogram_configuration.csv",
+                    data=analysis.legacy_spectrogram_configuration_csv().encode(
+                        "utf-8"
+                    ),
+                )
+            else:
+                zip_file.writestr(
+                    "create_analyse.py",
+                    data=f"""
+                    import numpy as np
+                    from osekit.public_api.analysis import Analysis, AnalysisType
+                    from pandas import Timestamp, Timedelta
+                    from scipy.signal import ShortTimeFFT
+                    
+                    fft = ShortTimeFFT(
+                        mfft={analysis.fft.nfft},
+                        win=np.array({list([int(item) for item in analysis.fft.window])}),
+                        hop={round(analysis.fft.window_size * (1 - analysis.fft.overlap))},
+                        fs={analysis.fft.sampling_frequency},
+                        scale_to={analysis.fft.scaling if analysis.fft.scaling in ["magnitude", "psd"] else None},
+                    )
+                    
+                    Analysis(
+                        analysis_type=AnalysisType.SPECTROGRAM,  # Spectro only
+                        begin=Timestamp.fromisoformat({analysis.start_date.isoformat()}),
+                        end=Timestamp.fromisoformat({analysis.end_date.isoformat()}),
+                        data_duration=Timedelta(f"{analysis.data_duration}s"),
+                        sample_rate={analysis.fft.sampling_frequency},
+                        name="{analysis.name}",
+                        fft=fft,
+                        colormap={analysis.colormap.name},
+                    )
+                    """.encode(
+                        "utf-8"
+                    ),
+                )
 
         response = HttpResponse(content_type="application/x-zip-compressed")
         response["Content-Disposition"] = f"attachment; filename={analysis.name}.zip"
