17b1b1759a7fdef553f2e6ebb84a706f46d5a847
Elodie <150901577+ElodieENSTA@users.noreply.github.com>
2026-09-09 11:30:23 +0200

    feat(annotator): Labels shortcuts used on selected annotation should either update label (strong) or remove (weak w/ same label) annotation (#492)
    * feat(annotator): Labels shortcuts used on selected strong annotation should update annotation label
    
    * feat(annotator): Labels shortcuts used on selected weak annotation with same label should remove annotation
    
    * clean: duplicated code

--- frontend/src/features/Annotator/Label/LabelChip.tsx
+++ frontend/src/features/Annotator/Label/LabelChip.tsx
@@ -50,10 +50,29 @@ export const LabelChip: React.FC<{
             type: AnnotationType.Weak,
             label,
         }
+
         const weak = getAnnotation(weakProperties)
-        if (weak) return dispatch(focusAnnotation(weak))
-        addAnnotation({ ...weakProperties, confidence: defaultConfidence })
-    }, [ focusedAnnotation, updateAnnotation, label, getAnnotation, dispatch, addAnnotation, defaultConfidence ])
+        if (!weak) {
+            // Create weak if it doesn't exist
+            addAnnotation({ ...weakProperties, confidence: defaultConfidence })
+        }
+
+        if (focusedAnnotation) {
+            // If focused annotation exists
+            if (focusedAnnotation.type !== AnnotationType.Weak) {
+                // If focused annotation is strong => update its label
+                updateAnnotation(focusedAnnotation, { label })
+            } else if (focusedAnnotation.label === label) {
+                // If focused annotation is weak and has selected label => remove weak annotation
+                removeAnnotation(focusedAnnotation)
+                return
+            }
+        }
+        if (weak) {
+            // If there is no focused strong annotation: focus existing weak annotation
+            dispatch(focusAnnotation(weak))
+        }
+    }, [ focusedAnnotation, updateAnnotation, label, getAnnotation, dispatch, addAnnotation, defaultConfidence, removeAnnotation ])
     useHotkeySequence(numberShortcuts, () => select())
     useHotkeySequence(keyShortcuts, () => select())
 
